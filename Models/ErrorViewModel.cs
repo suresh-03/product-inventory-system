8e87ebfdc7ea10a96cb77ad4ec416b1aac4c2d0f
suresh-03 <sureshkumar732003@gmail.com>
2025-04-24 11:54:29 +0530

    project setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace product_inventory_system.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
